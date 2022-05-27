ce1b77a70f62fb80deffe5bd732118732156c91a
Florian Klampfer <mail@qwtel.com>
2022-05-27 16:50:13 +0700

    Commit

--- utils/common-types.ts
+++ utils/common-types.ts
@@ -4,6 +4,10 @@ export type Callable<T> = T | (() => T);
 export type Primitive = null | undefined | boolean | number | string | bigint | symbol;
 export type ToString = { toString(...args: any[]): string }
 
+export type TypedEventListener<E extends Event> = (evt: E) => void | Promise<void>;
+export type TypedEventListenerObject<E extends Event> = { handleEvent(evt: E): void | Promise<void>; }
+export type TypedEventListenerOrEventListenerObject<E extends Event> = TypedEventListener<E> | TypedEventListenerObject<E>;
+
 // /** See: https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm */
 // export type StructuredCloneable = Omit<Primitive, symbol>
 //   | Date 
